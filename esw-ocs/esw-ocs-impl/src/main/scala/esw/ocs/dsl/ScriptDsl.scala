1e058cdb9a542205a2f8ce6248f64f07f22d0a8d
Pritam Kadam <kpritam@thoughtworks.com>
2019-08-30 15:48:35 +0530

    rever ScriptDsl

--- esw-ocs/esw-ocs-impl/src/main/scala/esw/ocs/dsl/ScriptDsl.scala
+++ esw-ocs/esw-ocs-impl/src/main/scala/esw/ocs/dsl/ScriptDsl.scala
@@ -1,22 +1,18 @@
 package esw.ocs.dsl
 
-import java.util.concurrent.{CompletableFuture, CompletionStage}
-
 import akka.Done
 import csw.params.commands.{Observe, SequenceCommand, Setup}
 import esw.ocs.api.models.responses.PullNextResult
 import esw.ocs.dsl.utils.{FunctionBuilder, FunctionHandlers}
 import esw.ocs.exceptions.UnhandledCommandException
 
-import scala.compat.java8.FutureConverters.CompletionStageOps
 import scala.concurrent.Future
 import scala.concurrent.duration.DurationDouble
-import scala.jdk.FutureConverters.FutureOps
 import scala.reflect.ClassTag
 
 class Script(val csw: CswServices) extends ScriptDsl {
   // todo: should this come from conf file?
-  override val loopInterval = 50.millis
+  override private[ocs] val loopInterval = 50.millis
 }
 
 trait ScriptDsl extends ControlDsl {
@@ -24,17 +20,17 @@ trait ScriptDsl extends ControlDsl {
 
   var isOnline = true
 
-  val commandHandlerBuilder: FunctionBuilder[SequenceCommand, Future[Unit]] = new FunctionBuilder
+  private val commandHandlerBuilder: FunctionBuilder[SequenceCommand, Future[Unit]] = new FunctionBuilder
 
-  val onlineHandlers: FunctionHandlers[Unit, Future[Unit]]   = new FunctionHandlers
-  val offlineHandlers: FunctionHandlers[Unit, Future[Unit]]  = new FunctionHandlers
-  val shutdownHandlers: FunctionHandlers[Unit, Future[Unit]] = new FunctionHandlers
-  val abortHandlers: FunctionHandlers[Unit, Future[Unit]]    = new FunctionHandlers
+  private val onlineHandlers: FunctionHandlers[Unit, Future[Unit]]   = new FunctionHandlers
+  private val offlineHandlers: FunctionHandlers[Unit, Future[Unit]]  = new FunctionHandlers
+  private val shutdownHandlers: FunctionHandlers[Unit, Future[Unit]] = new FunctionHandlers
+  private val abortHandlers: FunctionHandlers[Unit, Future[Unit]]    = new FunctionHandlers
 
-  def handle[T <: SequenceCommand: ClassTag](name: String)(handler: T => Future[Unit]): Unit =
+  private def handle[T <: SequenceCommand: ClassTag](name: String)(handler: T => Future[Unit]): Unit =
     commandHandlerBuilder.addHandler[T](handler)(_.commandName.name == name)
 
-  lazy val commandHandler: SequenceCommand => Future[Unit] =
+  private lazy val commandHandler: SequenceCommand => Future[Unit] =
     commandHandlerBuilder.build { input =>
       // should script writer have ability to add this default handler, like handleUnknownCommand
       spawn {
@@ -42,25 +38,24 @@ trait ScriptDsl extends ControlDsl {
       }
     }
 
-  def execute(command: SequenceCommand): Future[Unit]           = spawn(commandHandler(command).await)
-  def jExecute(command: SequenceCommand): CompletionStage[Void] = spawn(commandHandler(command).await).asJava.thenAccept(_ => ())
+  private[ocs] def execute(command: SequenceCommand): Future[Unit] = spawn(commandHandler(command).await)
 
-  def executeGoOnline(): Future[Done] =
+  private[ocs] def executeGoOnline(): Future[Done] =
     Future.sequence(onlineHandlers.execute(())).map { _ =>
       isOnline = true
       Done
     }
 
-  def executeGoOffline(): Future[Done] = {
+  private[ocs] def executeGoOffline(): Future[Done] = {
     isOnline = false
     Future.sequence(offlineHandlers.execute(())).map(_ => Done)
   }
 
-  def executeShutdown(): Future[Done] = Future.sequence(shutdownHandlers.execute(())).map(_ => Done)
+  private[ocs] def executeShutdown(): Future[Done] = Future.sequence(shutdownHandlers.execute(())).map(_ => Done)
 
-  def executeAbort(): Future[Done] = Future.sequence(abortHandlers.execute(())).map(_ => Done)
+  private[ocs] def executeAbort(): Future[Done] = Future.sequence(abortHandlers.execute(())).map(_ => Done)
 
-  def nextIf(f: SequenceCommand => Boolean): Future[Option[SequenceCommand]] =
+  protected final def nextIf(f: SequenceCommand => Boolean): Future[Option[SequenceCommand]] =
     spawn {
       val operator  = csw.sequenceOperatorFactory()
       val mayBeNext = operator.maybeNext.await
@@ -74,18 +69,10 @@ trait ScriptDsl extends ControlDsl {
       }
     }
 
-  def handleSetupCommand(name: String)(handler: Setup => Future[Unit]): Unit = handle(name)(handler)
-  def jHandleSetupCommand(name: String)(handler: Setup => CompletableFuture[Void]): Void = {
-    handle(name)((command: Setup) => handler(command).toScala.map(_ => ()))
-    null
-  }
-
-  def handleObserveCommand(name: String)(handler: Observe => Future[Unit]): Unit = handle(name)(handler)
-//  def jHandleObserveCommand(name: String)(handler: Observe => CompletableFuture[Unit]): Unit =
-//    handle(name)((command: Observe) => handler(command).toScala)
-
-  def handleGoOnline(handler: => Future[Unit]): Unit  = onlineHandlers.add(_ => handler)
-  def handleGoOffline(handler: => Future[Unit]): Unit = offlineHandlers.add(_ => handler)
-  def handleShutdown(handler: => Future[Unit]): Unit  = shutdownHandlers.add(_ => handler)
-  def handleAbort(handler: => Future[Unit]): Unit     = abortHandlers.add(_ => handler)
+  protected final def handleSetupCommand(name: String)(handler: Setup => Future[Unit]): Unit     = handle(name)(handler)
+  protected final def handleObserveCommand(name: String)(handler: Observe => Future[Unit]): Unit = handle(name)(handler)
+  protected final def handleGoOnline(handler: => Future[Unit]): Unit                             = onlineHandlers.add(_ => handler)
+  protected final def handleGoOffline(handler: => Future[Unit]): Unit                            = offlineHandlers.add(_ => handler)
+  protected final def handleShutdown(handler: => Future[Unit]): Unit                             = shutdownHandlers.add(_ => handler)
+  protected final def handleAbort(handler: => Future[Unit]): Unit                                = abortHandlers.add(_ => handler)
 }
